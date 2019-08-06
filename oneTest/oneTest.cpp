70d4dccba7c58b1e077c807b14594d899ed0793c
SirKy Dong <dsk_320@outlook.com>
2019-08-06 11:40:09 +0800

    new change

--- oneTest/oneTest.cpp
+++ oneTest/oneTest.cpp
@@ -2,7 +2,13 @@
 #include<iostream>
 #include"Sales_item.h"
 #include"Sales_data.h"
-
+#include<string>
+#include<cctype>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
 int main() {
 	//输入数组之和
 	/*int sum = 0, value = 0;
@@ -60,14 +66,46 @@ int main() {
 	//std::cout<<c<<std::endl;
 	//std::cout << d << std::endl;
 
-	Sales_data data1, data2;
+	//Sales_data data1, data2;
 
-	double price = 0;
-	std::cin >> data1.bookNo >> data1.units_sold >> price;
-	data1.revenue = data1.units_sold*price;
+	//double price = 0;
+	//std::cin >> data1.bookNo >> data1.units_sold >> price;
+	//data1.revenue = data1.units_sold*price;
 
-	std::cin >> data2.bookNo >> data2.units_sold >> price;
-	data2.revenue = data2.units_sold*price;
+	//std::cin >> data2.bookNo >> data2.units_sold >> price;
+	//data2.revenue = data2.units_sold*price;
+/*第三章 字符串、向量和数组*/
+	//string s5 = "    Hello World";
+	//string s6("adfg");
+	//string s7(10, 'c');
+	//string s8 = string(10, 'd');
+	//cout << s5 << endl << s6 << endl << s7 << endl<<s8<<endl;
+	//cin >> s5>>s6;
+	//cout << s5 <<s6<< endl;
+
+
+	//string word;
+	//while (cin >> word) {
+	//	cout << word << endl;
+
+	//}
+
+
+
+	string line;
+	while (getline(cin, line)) {
+		if (!line.empty()) {
+			cout << "the line size:" << line.size() << endl;
+			auto str_type = line.size();//.size()类型是string::size_type类型，无符号整型数
+			cout << str_type << endl;
+		}
+		else
+			cout << "error:the line is empty" << endl;
+	}
+
+
+	
+
 	return 0;
 }
 
