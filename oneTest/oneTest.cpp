c502c206dfee102ef5fbd51e7087ef7dfa57f3c7
SirKy Dong <dsk_320@outlook.com>
2019-08-16 17:22:09 +0800

    8.16

--- oneTest/oneTest.cpp
+++ oneTest/oneTest.cpp
@@ -180,6 +180,43 @@ int main() {
 
 
 
+	//vector<unsigned> scores(11, 0);
+	//unsigned grade;
+	//while (cin >> grade) {
+	//	if (grade <= 100) {
+	//		++scores[grade / 10];
+	//	}
+	//}
+	//for (auto i : scores) {
+	//	cout << i << " ";
+	//}
+	//cout << endl;
+
+
+
+	//vector<string> words;
+	//string word;
+	//while (cin >> word) {
+	//	if (!word.empty()) {
+	//		words.push_back(word);
+	//	}
+	//}
+	//for (auto s : words) {
+	//	for (char& c : s) {
+	//		c=toupper(c);
+	//	}
+	//	cout << s<<endl;
+	//}
+
+
+
+	//迭代器
+	string ss = "some string";
+	if (ss.begin() != ss.end()) {
+		auto it = ss.begin();
+		*it = toupper(*it);
+	}
+	cout << ss << endl;
 
 
 
